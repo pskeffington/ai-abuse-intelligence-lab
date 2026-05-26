915145233854c65fbbf95a1c60aa47a999a8473b
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-26 02:24:35 -0400

    Export graph analysis classes

--- src/ai_abuse_intel_lab/analysis/__init__.py
+++ src/ai_abuse_intel_lab/analysis/__init__.py
@@ -1,5 +1,6 @@
 """Analysis components."""
 
 from ai_abuse_intel_lab.analysis.coordination import CoordinationSignalAnalyzer
+from ai_abuse_intel_lab.analysis.graph import EventGraphBuilder, EventGraphSummary
 
-__all__ = ["CoordinationSignalAnalyzer"]
+__all__ = ["CoordinationSignalAnalyzer", "EventGraphBuilder", "EventGraphSummary"]
