45d8d56af552ff75acd2b899d5460d5e6822e9fa
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-30 23:06:07 -0400

    Export analysis pipeline

--- src/ai_abuse_intel_lab/analysis/__init__.py
+++ src/ai_abuse_intel_lab/analysis/__init__.py
@@ -3,8 +3,11 @@
 from ai_abuse_intel_lab.analysis.burst import BurstSignalAnalyzer, BurstWindow
 from ai_abuse_intel_lab.analysis.coordination import CoordinationSignalAnalyzer
 from ai_abuse_intel_lab.analysis.graph import EventGraphBuilder, EventGraphSummary
+from ai_abuse_intel_lab.analysis.pipeline import AnalysisPipeline, AnalysisPipelineConfig
 
 __all__ = [
+    "AnalysisPipeline",
+    "AnalysisPipelineConfig",
     "BurstSignalAnalyzer",
     "BurstWindow",
     "CoordinationSignalAnalyzer",
