bcb3fce8ba459f0ce49a632b1d728cf193394dd5
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-30 22:56:57 -0400

    Export burst analysis classes

--- src/ai_abuse_intel_lab/analysis/__init__.py
+++ src/ai_abuse_intel_lab/analysis/__init__.py
@@ -1,6 +1,13 @@
 """Analysis components."""
 
+from ai_abuse_intel_lab.analysis.burst import BurstSignalAnalyzer, BurstWindow
 from ai_abuse_intel_lab.analysis.coordination import CoordinationSignalAnalyzer
 from ai_abuse_intel_lab.analysis.graph import EventGraphBuilder, EventGraphSummary
 
-__all__ = ["CoordinationSignalAnalyzer", "EventGraphBuilder", "EventGraphSummary"]
+__all__ = [
+    "BurstSignalAnalyzer",
+    "BurstWindow",
+    "CoordinationSignalAnalyzer",
+    "EventGraphBuilder",
+    "EventGraphSummary",
+]
