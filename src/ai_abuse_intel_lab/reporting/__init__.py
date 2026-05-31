8edb27e440ccf4ddc152e8b2ea315229afd8d493
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-31 01:45:16 -0400

    Export JSON reporter

--- src/ai_abuse_intel_lab/reporting/__init__.py
+++ src/ai_abuse_intel_lab/reporting/__init__.py
@@ -1,5 +1,6 @@
 """Reporting helpers."""
 
+from ai_abuse_intel_lab.reporting.json import JsonFindingReporter
 from ai_abuse_intel_lab.reporting.markdown import MarkdownFindingReporter
 
-__all__ = ["MarkdownFindingReporter"]
+__all__ = ["JsonFindingReporter", "MarkdownFindingReporter"]
